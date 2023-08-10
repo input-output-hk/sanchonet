107b87fd7a0fce17a1884d63aff308634d34ca8d
Brendan Moore <brendan.moore@iohk.io>
2023-08-10 12:20:01 +0100

    feat: Animate navbar bg color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React, { useEffect } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import { useScroll, useTransform } from "framer-motion";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/get-started">
+            to="/get-started"
+          >
             Get Started
           </Link>
         </div>
@@ -27,10 +29,26 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
+  const { scrollY } = useScroll();
+  const y = useTransform(
+    scrollY,
+    [0, 100],
+    ["rgba(1, 107, 130, 0)", "rgba(1, 107, 130, 0.96)"]
+  );
+
+  useEffect(() => {
+    const el = document.querySelector(".navbar") as HTMLElement;
+    return y.onChange((v) => {
+      if (el) {
+        el.style.backgroundColor = v;
+      }
+    });
+  }, []);
+
   return (
-    <Layout
-      description={siteConfig.tagline}>
+    <Layout description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
